c41f352b69291f8635fc43f497a094351218ad04
Quang Phu Duc Vo <105739480@student.swin.edu.au>
2026-07-03 12:59:18 +1000

    Learning Recursion

---
+++ Maths, Patterns and Recursion/Recursion.py
@@ -0,0 +1,19 @@
+def factorial (n):
+    if n == 0  or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+    
+
+a = factorial(5)
+
+print (a)
+
+b = factorial(0)
+
+print (b)
+
+c = factorial(1)
+
+print (c)
+
