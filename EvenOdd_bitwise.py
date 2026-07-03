3e046537fa4cd0cf853e339f94fd79a53b3f61f3
Quang Phu Duc Vo <105739480@student.swin.edu.au>
2026-07-03 13:05:02 +1000

    New Knowledge

---
+++ EvenOdd_bitwise.py
@@ -0,0 +1,14 @@
+def BitwiseEvenOdd(num):
+    if num & 1 == 0:
+        return "Even"
+    else:
+        return "Odd"
+    
+
+a1 = BitwiseEvenOdd(4)
+print (a1)
+
+a2 = BitwiseEvenOdd(7)
+
+
+print (a2)
